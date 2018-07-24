9999f56ebe8c4bc4865d00a91991d227650ac614
hyacinth <hyacinth.c@3wp.io>
2018-07-24 12:12:50 +0100

    error handling bug fix

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -1,23 +1,22 @@
 Template.postSubmit.events({
-   'submit form': function(event){
-      event.preventDefault();
-      const target = event.target;
+    'submit form': function (event) {
+        event.preventDefault();
+        const target = event.target;
 
-      var post = {
-          url: target.url.value,
-          title: target.title.value,
+        var post = {
+            url: target.url.value,
+            title: target.title.value,
         };
 
-      Meteor.call('postInsert', post, function(error, result){
-          if (error){
-              return alert(error.reason);
-              if (result.postExists){
-                  alert('This link has already been posted');
-                }
-            }else{
-              Router.go('postPage', { _id: result._id } );
+        Meteor.call('postInsert', post, function (error, result) {
+            if (error) {
+                return throwError(error.reason);
             }
+            if (result.postExists) {
+                throwError('This link has already been posted');
+            }
+            Router.go('postPage', { _id: result._id });
         });
-     
+
     }
 });
